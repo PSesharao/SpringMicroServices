48a17d3d95137dddbf956931a23ff754ec9c4c5b
Sesharao Paritala <sesharao.paritala2@mindtree.com>
2021-03-19 11:50:13 +0530

    deleting all

--- spring-microservices-cloud1/src/main/java/com/mindtree/msscbrewery/services/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.mindtree.msscbrewery.services;
-
-import java.util.UUID;
-
-import com.mindtree.msscbrewery.web.model.CustomerDto;
-
-public interface CustomerService {
-	CustomerDto getCustomerById(UUID customerId);
-}
